fc83ba79455b6088ce0d3c4eb17a742e9cf5c743
Vinayak Sachdeva <vinayak.sachdeva@codenation.co.in>
2024-06-07 07:13:48 +0000

    2024-06-07_07-09-01

--- src/test/java/io/socket/backo/BackoffTest.java
+++ src/test/java/io/socket/backo/BackoffTest.java
@@ -25,25 +25,25 @@ public class BackoffTest {
 
     @Test
     public void durationOverflow() {
-        for (int i = 0; i < 10; i++) {
+        IntStream.range(0, 10).forEach(i -> {
             Backoff b = new Backoff();
             b.setMin(100);
             b.setMax(10000);
             b.setJitter(0.5);
-
-            // repeats to make it overflow (a long can have 2 ** 63 - 1)
-            for (int j = 0; j < 100; j++) {
+            IntStream.range(0, 100).forEach(j -> {
                 BigInteger ms = BigInteger.valueOf(100).multiply(BigInteger.valueOf(2).pow(j));
                 BigInteger deviation = new BigDecimal(ms).multiply(BigDecimal.valueOf(0.5)).toBigInteger();
                 BigInteger duration = BigInteger.valueOf(b.duration());
-
                 BigInteger min = ms.subtract(deviation).min(BigInteger.valueOf(10000));
+
                 BigInteger max = ms.add(deviation).min(BigInteger.valueOf(10001));
                 assertTrue(min + " <= " + duration + " < " + max,
                         min.compareTo(duration) <= 0 && max.compareTo(duration) == 1);
-            }
-        }
+            });
+        });
     }
+    }
+}
 
     @Test(expected = IllegalArgumentException.class)
     public void ensureJitterIsValid() {
